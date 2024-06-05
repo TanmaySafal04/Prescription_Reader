b63425f0ee39b1da2854805dc8eb41d8d7558bbe
TanmaySafal04 <tanmaysafal.4@gmail.com>
2024-06-05 19:35:35 +0530

    Removed dotenv from setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.0.1',
     author='Tanmay safal',
     author_email='tanmaysafal.4@gmail.com',
-    install_requires=["gTTS","langchain","streamlit","python-dotenv","PyPDF2","dotenv","paddlepaddle","paddleocr","streamlit","transformers","langsmith","langchain_community"],
+    install_requires=["gTTS","langchain","streamlit","python-dotenv","PyPDF2","paddlepaddle","paddleocr","streamlit","transformers","langsmith","langchain_community"],
     packages=find_packages()
 )
